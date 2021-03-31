dd762de84f01e96c1e2f260a93a75f9c7934c019
doreti <sanchose98@yandex.ru>
2021-03-31 23:09:48 +0300

    add chat

--- template/js.js
+++ template/js.js
@@ -14,6 +14,39 @@ document.querySelector("#resButton").onclick = function(){
 	//alert(res);
 };
 
+function sendMessage () {
+		let xhrObj = new XMLHttpRequest();
+		let text = textArea.value;
+		let name = userName.value;
+		console.log(text);
+		//console.log(name);
+		xhrObj.open('GET', 'http://chat21.std-400.ist.mospolytech.ru/add.ajax.php?name=+name+&text=+text');
+		xhrObj.send();
+		if(xhrObj.response){
+			let result = JSON.parse(xhrObj.response);
+			console.log(result);
+			
+		}
+		/*
+		if (xhrObj.status != 200) {
+		// обработать ошибку
+			//alert( xhrObj.status + ': ' + xhrObj.statusText ); // пример вывода: 404: Not Found
+		} else {
+		// вывести результат
+			//alert( xhrObj.responseText ); // responseText -- текст ответа.
+		}
+		*/
+		
+		
+};
+
+let button1 = document.getElementById('sendButton');
+let userName = document.getElementById('vvName');
+let textArea = document.getElementById('vvText');
+//sendButton.onclick =  sendMessage();
+button1.addEventListener('click',sendMessage);
+
+
 /*
 
 #	alert(firstChislo);
